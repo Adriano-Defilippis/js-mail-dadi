97347f39dc1278707ce4ec0bf3c107d0f023b4ee
Adriano-Defilippis <ciprianoadrianodefilippis@gmail.com>
2019-07-02 16:35:32 +0200

    Attraversamento Array con ciclo ed if per comparazione input con Elemento Array. Problemi: le istruzioni in else vengono ciclate per la lunghezza dell'array

--- script.js
+++ script.js
@@ -7,7 +7,16 @@ mail = prompt('inserisci la tua mail')
 // Check mail list
 var mioarray = ["luca.luca@gmail.com", "paolo.paolo@hotmail.com", "nicola@gmail.it", "francesco@aruba.it" , "andrea.andrea@tiscali.it", "mario.luca@yahoo.com", "gildo@gmail.com"];
 
-console.log(mioarray);
 
 
-// Retur accesso concesso o negato
+// Controllo nell'Array se la mail inserita dall'utente è presente
+for (i = 0; i < mioarray.length; i++){
+  if (mail===mioarray[i]){
+    alert('Benvenuto');
+    console.log(mioarray[i]);
+  }
+  else{
+    alert('accesso negato per questo utente');
+    console.log(mioarray[i]);
+  }
+}
