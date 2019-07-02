21032ac6124d535829035ce1336846847e66656c
Adriano-Defilippis <ciprianoadrianodefilippis@gmail.com>
2019-07-03 00:15:19 +0200

    Creato controllo di vincita nel caso il dado lanciato dall'utente sia più grande di quello della Cpu. Creata anche casistica di parità

--- dadi/script.js
+++ dadi/script.js
@@ -1,17 +1,37 @@
 // creo la funzine per il lancio dei dadi
 function lancioDadoUtente() {
-   var dadonum, risultato;
+   var dadonum, dadocpu, esito;
 
-   // Genero un numero random da 1 a 6
-   dadonum = Math.floor((Math.random() * 6)+ 1);
-   console.log(dadonum);
+   // Genero un numero random da 1 a 6 per l'utente
+   dadoutente = Math.floor((Math.random() * 6)+ 1);
+   // Genero un numero random da 1 a 6 per la cpu
+   dadocpu = Math.floor((Math.random() * 6)+ 1);
+   console.log(dadocpu);
+   // Valorizzo la variabile esito della sfida
+   esito = document.getElementById('esito');
 
-   risultato = document.getElementById('risultato_utente');
-   risultato.innerHTML = "il numero uscito per l'utente é: " + dadonum;
-   console.log(dadonum);
+   // Stampo i numeri random a video
+   document.getElementById('risultato_utente').innerHTML = "il numero uscito per l'utente é: " + dadoutente;
+
+   document.getElementById('risultato_cpu').innerHTML = "il numero uscito per la cpu è: " + dadocpu;
+
+   //controllo vincitore
+   if (dadoutente > dadocpu){
+     esito.innerHTML = "hai vinto!"
+
+   }
+   else if (dadoutente == dadocpu) {
+     esito.innerHTML = "Parità!"
+
+   }
+   else{
+     esito.innerHTML = "non hai vinto!"
+   }
 
 }
 
-var button = document.getElementById('button');
+var button;
+button = document.getElementById('button');
 
+// Azione onclick sul button
 button.onclick = lancioDadoUtente;
