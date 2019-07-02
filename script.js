914e0e811b6c84df338af411144674e83eb12fa4
Adriano-Defilippis <ciprianoadrianodefilippis@gmail.com>
2019-07-02 16:04:42 +0200

    Inserito Array per lista delle mail

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 // Chiedi la mail all'utente
+var mail;
+
+mail = prompt('inserisci la tua mail')
 
 
 // Check mail list
+var mioarray = ["luca.luca@gmail.com", "paolo.paolo@hotmail.com", "nicola@gmail.it", "francesco@aruba.it" , "andrea.andrea@tiscali.it", "mario.luca@yahoo.com", "gildo@gmail.com"];
+
+console.log(mioarray);
 
 
 // Retur accesso concesso o negato
